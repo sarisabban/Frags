7250a0f4e630ffc153a06bdb211e53d66357c7d9
Sari Sabban <sari.sabban@gmail.com>
2017-09-04 23:59:39 +0300

    Update Frags.py

--- Frags.py
+++ Frags.py
@@ -11,39 +11,19 @@ Vall = sys.argv[2]
 def MakeLocal(pose):
 	''' Preforms fragment picking and secondary structure prediction locally '''
 	''' Generates the 3-mer file, the 9-mer file, and the PsiPred file '''
-	#Generate blueprint file
-	filename = 'structure.pdb'
-	sslist=list()
-	p = Bio.PDB.PDBParser()
-	structure = p.get_structure('X', filename)
-	model = structure[0]
-	dssp = Bio.PDB.DSSP(model, filename)
-	for x in dssp:
-		if x[2]=='G' or x[2]=='H' or x[2]=='I':
-			y = x[1] + ' ' + 'H'
-		elif x[2]=='B' or x[2]=='E':
-			y= x[1] + ' ' + 'S'
-		else:
-			y= x[1] + ' ' + 'L'
-		sslist.append(y)
-	blueprintfile = open('blueprint' , 'w')
-	count = 0
-	for ss in sslist:
-		count += 1
-		blueprintfile.write(str(count) + ' ' + ss + '\n')
-	blueprintfile.close()
 	#Generate FASTA file
 	sequence = pose.sequence()
-	fasta = open(sys.argv[1] + '.fasta' , 'w')
+	filename = sys.argv[1].split('.')
+	fasta = open(filename[0] + '.fasta' , 'w')
 	fasta.write(sequence)
 	fasta.close()
-	#Generate PSIPRED prediction file
-	psipred = pyrosetta.rosetta.core.io.external.PsiPredInterface(' ')# <------- PROBLEM
-	psipred.run_psipred(pose , 'blueprint')# <------- PROBLEM
-	os.remove('blueprint')
+	#Generate PSIPRED prediction file (http://bioinfadmin.cs.ucl.ac.uk/downloads/psipred/)
+#	os.system('')
+	#Generate Checkpoint file (ftp://ftp.ncbi.nih.gov/blast/executables/blast+/LATEST/)
+#	os.system('')
 	#Generate fragment files
 	for frag in [3 , 9]:
-		init('-in::file::fasta structure.fasta -in::file::s structure.pdb -frags::frag_sizes ' + str(frag) + ' -frags::n_candidates 1000 -frags:write_ca_coordinates -frags::n_frags 200')
+		init('-in::file::fasta ' + filename[0] + '.fasta' + ' -in::file::s ' + sys.argv[1] + ' -frags::frag_sizes ' + str(frag) + ' -frags::n_candidates 1000 -frags:write_ca_coordinates -frags::n_frags 200')
 		fregment = pyrosetta.rosetta.protocols.frag_picker.FragmentPicker()
 		fregment.parse_command_line()
 		fregment.read_vall(Vall)
